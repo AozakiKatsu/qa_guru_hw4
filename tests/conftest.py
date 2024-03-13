7fd644ab7b6a322840503f1659980fd1cbf423c1
Irina Luchkova <shtejn@gmail.com>
2024-03-13 13:04:47 +0300

    conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,16 @@
-from selene import browser
-import pytest
-from selenium import webdriver
-
-
-@pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    browser.config.base_url = 'https://demoqa.com'
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
+# from selene import browser
+# import pytest
+# from selenium import webdriver
+#
+#
+# @pytest.fixture(scope='function', autouse=True)
+# def browser_management():
+#     browser.config.base_url = 'https://demoqa.com'
+#     driver_options = webdriver.ChromeOptions()
+#     driver_options.page_load_strategy = 'eager'
+#     browser.config.driver_options = driver_options
+#     browser.config.window_width = 1920
+#     browser.config.window_height = 1080
 # driver_options.add_argument('--headless')
 import pytest
 
